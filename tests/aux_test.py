f796cb7e0aeea06f4a9d174d30255623ad2ab4f1
ariniesabela <tschertkowa.alex@gmail.com>
2021-11-15 21:47:25 +0700

    delete zero yield test

--- tests/aux_test.py
+++ tests/aux_test.py
@@ -5,8 +5,6 @@ from portfolio_analyzer.aux import calculate_yield
 def test_calculate_yield():
     res = calculate_yield([5], [20], 5.100955724302702)
     assert res == pytest.approx(0.44025, 1e-4)
-    res = calculate_yield([5, 10, 40], [20, 6, 9], 55)
-    assert res == pytest.approx(.0, 1e-4)
     res = calculate_yield([1, 2, 3, 4, 5], [20, 21, 22, 23, 24], 10)
     assert res < 0
     res = calculate_yield([5000, 3000, 1000], [360, 153, 5], 7056.95545014398)
